a035ddfbeff62572c2f8754389ff4e363f08840b
Michael Dougal <michaeldougal@yahoo.com>
2023-11-28 10:28:16 -0700

    chore: added version to Get

--- src/shared/lib/Get.lua
+++ src/shared/lib/Get.lua
@@ -1,23 +1,23 @@
 --[[
 	Author: ChiefWildin
 	Module: Get.lua
-	Created: 12/05/2022
+	Version: 1.0.0
 
 	A utility module that provides functions that make it easier to get multiple
 	objects in one call.
 --]]
 
---Takes an `Instance` value followed by any number of strings that represent
---child `Instance` names and returns the final child. If the last argument is a
---table, it will return a tuple of the children specified by that table.
+-- Takes an `Instance` value followed by any number of strings that represent
+-- child `Instance` names and returns the final child. If the last argument is a
+-- table, it will return a tuple of the children specified by that table.
 --
---Examples:
---```lua
------ Single instance
---local keystone = Get(workspace, "Arch", "Keystone")
------ Multiple children
---local keystone, door = Get(workspace, "Arch", { "Keystone", "Door" })
---```
+-- Examples:
+-- ```lua
+-- -- Single instance
+-- local keystone = Get(workspace, "Arch", "Keystone")
+-- -- Multiple children
+-- local keystone, door = Get(workspace, "Arch", { "Keystone", "Door" })
+-- ```
 ---@param initialObject Instance The object to start navigating from.
 return function(initialObject: Instance, ...): ...Instance
 	local object = initialObject
